8872323da918f2fbdf19419fd1b64109caa78501
misaeliana <69031743+misaeliana@users.noreply.github.com>
2022-04-27 18:54:15 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MOBDEVEInternshipConnectMisaSo
+ 
---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/misa/eliana/mobdeve_internshipconnect_misa_so/MainActivity.kt
@@ -0,0 +1,11 @@
+package ph.edu.dlsu.mobdeve.misa.eliana.mobdeve_internshipconnect_misa_so
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
