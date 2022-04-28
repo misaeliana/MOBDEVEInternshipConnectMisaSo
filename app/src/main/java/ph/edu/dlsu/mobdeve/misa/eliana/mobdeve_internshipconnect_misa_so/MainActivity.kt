1b9279546e7cabfb62061ed8f122e504aa58dba6
tiffanyanneso <tiffany_anne_so@dlsu.edu.ph>
2022-04-28 15:12:43 +0800

    super simple register
    only accepts basic details (name, email, number, password)
    does not have dropdown for user type

--- app/src/main/java/ph/edu/dlsu/mobdeve/misa/eliana/mobdeve_internshipconnect_misa_so/MainActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/misa/eliana/mobdeve_internshipconnect_misa_so/MainActivity.kt
@@ -1,11 +1,18 @@
 package ph.edu.dlsu.mobdeve.misa.eliana.mobdeve_internshipconnect_misa_so
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_register.setOnClickListener {
+            val intent = Intent (this, Register::class.java)
+            startActivity (intent)
+        }
     }
 }
---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/misa/eliana/mobdeve_internshipconnect_misa_so/User.kt
@@ -0,0 +1,5 @@
+package ph.edu.dlsu.mobdeve.misa.eliana.mobdeve_internshipconnect_misa_so
+
+data class User(val name: String?=null, val email: String?=null, val number: Int?=null, val password:String?=null) {
+
+}
