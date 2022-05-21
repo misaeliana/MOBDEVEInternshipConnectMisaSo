8338339c567e9af33e990768cf2c57148f206f4e
tiffanyanneso <tiffany_anne_so@dlsu.edu.ph>
2022-05-21 13:50:12 +0800

    sidebar on intern view companies

--- app/src/main/java/ph/edu/dlsu/mobdeve/misa/eliana/mobdeve_internshipconnect_misa_so/intern/InternViewCompanies.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/misa/eliana/mobdeve_internshipconnect_misa_so/intern/InternViewCompanies.kt
@@ -1,9 +1,14 @@
 package ph.edu.dlsu.mobdeve.misa.eliana.mobdeve_internshipconnect_misa_so.intern
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import ph.edu.dlsu.mobdeve.misa.eliana.mobdeve_internshipconnect_misa_so.MainActivity
+import ph.edu.dlsu.mobdeve.misa.eliana.mobdeve_internshipconnect_misa_so.R
 import ph.edu.dlsu.mobdeve.misa.eliana.mobdeve_internshipconnect_misa_so.adapter.CompanyAdapter
 import ph.edu.dlsu.mobdeve.misa.eliana.mobdeve_internshipconnect_misa_so.adapter.InternMyInternshipsAdapter
 import ph.edu.dlsu.mobdeve.misa.eliana.mobdeve_internshipconnect_misa_so.dao.CompaniesDAO
@@ -21,11 +26,14 @@ class InternViewCompanies : AppCompatActivity() {
 
     //private lateinit var dbref:DatabaseReference
 
+    lateinit var toggle: androidx.appcompat.app.ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInternViewCompaniesBinding.inflate(layoutInflater)
         setContentView(binding.root)
         init()
+        sidebar()
 
         binding.rvCompanyList.setLayoutManager(LinearLayoutManager(applicationContext))
         // binding.rvList.setLayoutManager(GridLayoutManager(getApplicationContext(), 2))
@@ -34,6 +42,12 @@ class InternViewCompanies : AppCompatActivity() {
         binding.rvCompanyList.setAdapter(companyAdapter)
 
         //getCompanyData()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item))
+            return true
+        return super.onOptionsItemSelected(item)
     }
 
     private fun init() {
@@ -86,4 +100,49 @@ class InternViewCompanies : AppCompatActivity() {
 //        })
 //
 //    }
+
+    private fun sidebar() {
+        toggle = androidx.appcompat.app.ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navInternSideMenu.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_internHome -> {
+                    val intent = Intent (this, InternMenu::class.java)
+                    startActivity (intent)
+                }
+
+                R.id.nav_viewInternships -> {
+                    val intent = Intent (this, InternViewInternships::class.java)
+                    startActivity (intent)
+                }
+
+                R.id.nav_internPastInternships -> {
+                    val intent = Intent (this, InternMyInternships::class.java)
+                    startActivity (intent)
+                }
+
+                R.id.nav_viewCompanies -> {
+                    val intent = Intent (this, InternViewCompanies::class.java)
+                    startActivity (intent)
+                }
+
+                R.id.nav_internProfile -> {
+                    val intent = Intent (this, InternProfile::class.java)
+                    startActivity (intent)
+                }
+
+                R.id.nav_internLogout -> {
+                    FirebaseAuth.getInstance().signOut()
+                    val intent = Intent (this, MainActivity::class.java)
+                    startActivity (intent)
+                    finish()
+                }
+            }
+            true
+        }
+    }
 }
